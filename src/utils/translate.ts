f30156fd5c0072826721d77a61d458efcfd2d72a
Vikash Upadhyay <vikash.up2109@gmail.com>
2022-10-16 02:25:29 +0530

    translate edit

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,8 +1,13 @@
 // import {myLang, otherLang} from "../../public/script.js";
-
-const TRANSLATE_API = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=hi&dt=t&q=";
+// import languageList from "../../public/languageList";
+// const TRANSLATE_API = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=hi&dt=t&q=";
 
 const translate = async (text: string) => {
+     const my_lang = await chrome.storage.sync.get('my_default_lang');
+     const sl = my_lang.my_default_lang;
+     const other_lang = await chrome.storage.sync.get('other_default_lang');
+     const tl = other_lang.other_default_lang;
+     const TRANSLATE_API = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sl}&tl=${tl}&dt=t&q=`;
      const response = await fetch(TRANSLATE_API + text);
      const data = await response.json();
      return data[0][0][0];
