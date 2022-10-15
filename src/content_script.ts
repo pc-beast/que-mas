9436f47f53ac0de5ff951ea5e2219c22018f018d
pc-beast <impavanesh@gmail.com>
2022-10-15 15:56:36 +0530

    fix: remove console errors

--- src/content_script.ts
+++ src/content_script.ts
@@ -6,7 +6,7 @@ const translateMessagesCallback = async (e: Event) => {
   const messageOut = document.querySelectorAll('.message-out .copyable-text .selectable-text span');
   const messages = [...messageOut, ...messagesIn];
   const filterMessages = messages.filter(message => message.classList.contains('translated') === false);
-  for (let i = filterMessages.length-1; i >= 0; i--) {
+  for (let i = filterMessages.length-1; i >= 0; i--) {    
     const message = filterMessages[i];
     const text = message.textContent;
     if (text && !message.classList.contains('translated')) {
@@ -19,14 +19,11 @@ const translateMessagesCallback = async (e: Event) => {
 }
 
 const translateInputCallback = async (e: MutationRecord[]) => {
-  const div = document.querySelector('[class="p357zi0d ktfrpxia nu7pwgvd kcgo1i74 sap93d0t gndfcl4n _1m68F"] div');
-  console.log(e[0]);
   const text = e[0].target.textContent;
   if (text) {
     const translatedText = await translate(text);
     // TODO: to be implemented in a way that is to preserve the cursor position
-    // console.log(translatedText);
-    div!.innerHTML = translatedText;
+    console.log(translatedText);
   }
 }
 
@@ -35,22 +32,7 @@ try {
     console.log('WhatsApp Web is ready');
     const chatAreaNode = document.querySelector('.app-wrapper-web > .two > div:nth-child(4)');
     chatAreaNode?.addEventListener('DOMNodeInserted', translateMessagesCallback);
-  })
-
-  onReadyElement('[data-testid="conversation-compose-box-input"] .selectable-text', () => {
-    // place where the new element is to be inserted
-    const inputNodeParent = document.querySelector('[class="p357zi0d ktfrpxia nu7pwgvd kcgo1i74 sap93d0t gndfcl4n _1m68F"]');
-    
-    const div = document.createElement('div');
-    // add classes to the p tag "selectable-text copyable-text"
-    div.classList.add('selectable-text', 'copyable-text', 'asdfasdfasdf');
-    div.innerText = "";
-    const inputNode = document.querySelector('[data-testid="conversation-compose-box-input"] .selectable-text span');
-    div.addEventListener('click', () => {
-      inputNode!.innerHTML = div?.innerHTML || '';
-    })
-    inputNodeParent!.appendChild(div);
-  })
+  });
 
   onReadyElement('[data-testid="conversation-compose-box-input"] .selectable-text span', () => {
     const inputNode = document.querySelector('[data-testid="conversation-compose-box-input"] .selectable-text span');
