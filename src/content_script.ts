ef20854f6778a691d3e8b9ae5c6caa54c3e50a2d
Pavnesh Chaturvedi <56963647+pc-beast@users.noreply.github.com>
2022-10-16 03:05:40 +0530

    feat: complete translate functionality (#2)
    * add basic frontend
    
    * script file changes
    
    * translate edit
    
    * popup changes
    
    * finally make input translate work
    
    Co-authored-by: Vikash Upadhyay <vikash.up2109@gmail.com>

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,6 +1,6 @@
 import { boxComponent } from './components/dot';
 import { desanitizeText, translate } from './utils/translate';
-import { changeChildrenEvent, onInnerHTMLChange, onReadyElement } from './utils/watchers';
+import { onInnerHTMLChange, onReadyElement, isDotPresent } from './utils/watchers';
 
 const translateMessagesCallback = async (e: Event) => {
   const messagesIn = document.querySelectorAll('.message-in .copyable-text .selectable-text span');
@@ -19,12 +19,11 @@ const translateMessagesCallback = async (e: Event) => {
   }
 }
 
+var translatedText = '';
 const translateInputCallback = async (e: MutationRecord[]) => {
   const text = e[0].target.textContent;
   if (text) {
-    const translatedText = await translate(text);
-    // TODO: to be implemented in a way that is to preserve the cursor position
-    console.log(translatedText);
+    translatedText = await translate(text);
   }
 }
 
@@ -40,12 +39,25 @@ try {
     onInnerHTMLChange(inputNode as Node, translateInputCallback);
   });
 
-  onReadyElement('footer [data-testid="compose-box"] span:nth-child(2) > div > div:nth-child(2) > div', () => {
-    const textbox = document.querySelector('footer [data-testid="compose-box"] span:nth-child(2) > div > div:nth-child(2) > div')
-    textbox?.classList.add('textbox_flex');
-    const qm_box = boxComponent();
-    textbox?.appendChild(qm_box);
-  });
+  // injecting dot component and event listener to the DOM
+  setInterval(() => {
+    if (!isDotPresent()) {
+      const textbox = document.querySelector('footer [data-testid="compose-box"] span:nth-child(2) > div > div:nth-child(2) > div')
+      textbox?.classList.add('textbox_flex');
+      const qm_box = boxComponent();
+      textbox?.appendChild(qm_box);
+
+      const translateButton = document.querySelector('.qm_box .left_text');
+      translateButton?.addEventListener('click', (e: Event) => {
+        const input: HTMLElement = document.querySelector('[data-testid="conversation-compose-box-input"]')!;
+        input.focus();
+        document.execCommand('selectAll');
+        setTimeout(() => {
+          document.execCommand('insertText', false, translatedText);
+        }, 100);
+      });
+    }
+  }, 1000);
 
 } catch (error) {
   console.error("Error: ", error);
--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,23 +1,30 @@
-const TRANSLATE_API = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=hi&dt=t&q=";
+// import {myLang, otherLang} from "../../public/script.js";
+// import languageList from "../../public/languageList";
+// const TRANSLATE_API = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=hi&dt=t&q=";
 
 const translate = async (text: string) => {
-  const response = await fetch(TRANSLATE_API + text);
-  const data = await response.json();
-  return data[0][0][0];
+     const my_lang = await chrome.storage.sync.get('my_default_lang');
+     const sl = my_lang.my_default_lang;
+     const other_lang = await chrome.storage.sync.get('other_default_lang');
+     const tl = other_lang.other_default_lang;
+     const TRANSLATE_API = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sl}&tl=${tl}&dt=t&q=`;
+     const response = await fetch(TRANSLATE_API + text);
+     const data = await response.json();
+     return data[0][0][0];
 }
 
 const sanitizeText = (text: string) => {
-  return text.replace(/(\r\n|\n|\r)/gm, "");
+     return text.replace(/(\r\n|\n|\r)/gm, "");
 }
 
 const desanitizeText = (text: string) => {
-  const links = text.match(/(https?:\/\/[^\s]+)/g);
-  if (links) {
-    links.forEach(link => {
-      text = text.replace(link, `<a href="${link}" target="_blank">${link}</a>`);
-    });
-  }
-  return text;
+     const links = text.match(/(https?:\/\/[^\s]+)/g);
+     if (links) {
+          links.forEach(link => {
+               text = text.replace(link, `<a href="${link}" target="_blank">${link}</a>`);
+          });
+     }
+     return text;
 }
 
-export {translate, sanitizeText, desanitizeText};
+export { translate, sanitizeText, desanitizeText };
